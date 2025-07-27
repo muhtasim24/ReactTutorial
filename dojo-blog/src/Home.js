d3f01664e1f892a5b178e6e877f42ecb6cff7baa
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-07-27 18:40:50 -0400

    handling click events

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,8 +1,18 @@
 
 function Home () {
+
+    const handleClick = (e) => {
+        console.log('hello ninja', e.target);
+    }
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name)
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('mario')}> Click me again</button>
         </div>
     );
 }
