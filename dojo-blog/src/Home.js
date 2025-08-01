9d6d047da4556408367222f963d1b6d89c354edd
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-07-31 22:48:09 -0400

    Learning useEffect Hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 
@@ -10,14 +10,23 @@ function Home () {
         { title: 'Web dev top tips', body: 'li', author: 'mario', id: 3},
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter( (blog) => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect( () => {
+        console.log('use effect ran');
+        console.log(blogs);
+    }, [ name ]);
+
     return (
         <div className="home">
             <BlogList blogs = { blogs } title = "All Blogs!" handleDelete = {handleDelete}/>
+            <button onClick={ () => setName('luigi')}>Change Name</button>
+            <p> { name } </p>
         </div>
     );
 }
