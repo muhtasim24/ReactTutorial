3b98d8f640bf1ee4e2215641ff6a908eb138a2e5
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-07-31 22:21:12 -0400

    passing functions as props

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -10,10 +10,14 @@ function Home () {
         { title: 'Web dev top tips', body: 'li', author: 'mario', id: 3},
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter( (blog) => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <BlogList blogs = { blogs } title = "All Blogs!" />
-            <BlogList blogs = { blogs.filter( (blog) => blog.author === 'mario' ) } title = "All Blogs!" />
+            <BlogList blogs = { blogs } title = "All Blogs!" handleDelete = {handleDelete}/>
         </div>
     );
 }
