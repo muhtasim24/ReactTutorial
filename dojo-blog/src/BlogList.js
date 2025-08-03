4ed9d2d999ec8e0b5ff6c17fe0da561ddf3c927b
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-08-03 13:41:50 -0400

    fetching data with useEffect and conditional templating

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,6 +1,6 @@
 
 
-const BlogList = ({blogs, title , handleDelete}) => {
+const BlogList = ({blogs, title}) => {
     // const blogs  = props.blogs;
     // const title = props.title;
     // destructure the props instead in the argument
@@ -13,7 +13,6 @@ const BlogList = ({blogs, title , handleDelete}) => {
                 <div className='blog-preview' key={ blog.id }>
                     <h2> { blog.title } </h2>
                     <p> Written by { blog.author } </p>
-                    <button onClick = { () => handleDelete(blog.id) } > Delete</button>
                 </div>
             ))}
         </div>
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,29 +4,25 @@ import BlogList from './BlogList';
 
 function Home () {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'li', author: 'mario', id: 1},
-        { title: 'welcome party', body: 'li', author: 'yoshu', id: 2},
-        { title: 'Web dev top tips', body: 'li', author: 'mario', id: 3},
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
     const [name, setName] = useState('mario');
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter( (blog) => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
     useEffect( () => {
-        console.log('use effect ran');
-        console.log(blogs);
-    }, [ name ]);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data);
+            });
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs = { blogs } title = "All Blogs!" handleDelete = {handleDelete}/>
-            <button onClick={ () => setName('luigi')}>Change Name</button>
-            <p> { name } </p>
+            {blogs && <BlogList blogs = { blogs } title = "All Blogs!"/>}
+
         </div>
     );
 }
