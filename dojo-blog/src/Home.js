28c45ff93f602888150844782d6bce314fdac427
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-08-04 20:22:31 -0400

    error checking for useEffect

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -6,21 +6,36 @@ function Home () {
 
     const [blogs, setBlogs] = useState(null);
 
+    const [isPending, setIsPending] = useState(true);
+
+    const [error, setError] = useState(null);
     const [name, setName] = useState('mario');
 
     useEffect( () => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if (!res.ok){
+                    throw Error('could not fetch data');
+                }
+                console.log(res);
                 return res.json();
             })
             .then(data => {
                 console.log(data);
                 setBlogs(data);
-            });
+                setIsPending(false);
+                setError(null);
+            })
+            .catch( err => {
+                setIsPending(false);
+                setError(err.message);
+            })
     }, []);
 
     return (
         <div className="home">
+            { error && <div> { error } </div>}
+            { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs = { blogs } title = "All Blogs!"/>}
 
         </div>
