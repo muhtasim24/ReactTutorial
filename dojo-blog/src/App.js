3898c2a5614de8af5bab2ac7f30fe9b2d04456e2
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-08-04 22:15:13 -0400

    getting started on routers path

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   const title = "Welcome to the new blog";
@@ -13,10 +14,15 @@ function App() {
         <Navbar/>
         <div className='content'>
           <Switch>
-            <Route path="/"></Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
             
           </Switch>
-        </div>
+        </div> 
 
       </div>
     </Router>
