72e3ce74b5b39388719fac2540121a45fbe33752
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-08-05 21:57:46 -0400

    using route parameters for details of different blogs

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,6 +2,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   const title = "Welcome to the new blog";
@@ -19,6 +20,9 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
             
           </Switch>
--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -8,24 +8,33 @@ const useFetch = (url) => {
     const [error, setError] = useState(null);
 
     useEffect( () => {
-        fetch(url)
-            .then(res => {
-                if (!res.ok){
-                    throw Error('could not fetch data');
-                }
-                console.log(res);
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                setData(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch( err => {
-                setIsPending(false);
-                setError(err.message);
-            })
+        const abortCont = new AbortController();
+        setTimeout( () => {
+            fetch(url, { signal: abortCont.signal })
+                .then(res => {
+                    if (!res.ok){
+                        throw Error('could not fetch data');
+                    }
+                    console.log(res);
+                    return res.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch( err => {
+                    if (err.name === "AbortError") {
+                        console.log('fetch aborted');
+                    } else {
+                    setIsPending(false);
+                    setError(err.message);
+                    }
+                })
+            }, 1000);
+
+            return () => abortCont.abort();
     }, [url]);
 
     return { data, isPending, error };
