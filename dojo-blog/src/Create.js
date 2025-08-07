b21f90173eed45124e3506ac4c07730457ea14dd
Muhtasim Mushfiq <muhtasim01@gmail.com>
2025-08-06 23:25:44 -0400

    handleSubmit

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -6,11 +6,18 @@ const Create = () => {
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
 
+    const handleSubmit = (e) => {
+        // prevent the default action of the form being refreshed 
+        e.preventDefault();
+        const blog = { title , body , author};
+
+        console.log(blog);
+    };
 
     return (
         <div className = "create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <label>Blog Title:</label>
                 <input 
                     type="text"
